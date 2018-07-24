10eefe46ad629a5bf5d5aecb80e5a627c8aa276a
anqi <anqizhao0216@gmail.com>
2018-07-24 02:07:09 -0700

    redis

--- week3/oj-server/services/socketService.js
+++ week3/oj-server/services/socketService.js
@@ -1,4 +1,4 @@
-// var redisClient = require('../modules/redisClient');
+var redisClient = require('../modules/redisClient');
 
 const TIMEOUT_IN_SECONDS = 3600;
 
@@ -13,40 +13,40 @@ module.exports = function(io){
 		var sessionID = socket.handshake.query['sessionID'];
 		socketIDtoSessionID[socket.id] = sessionID;
 
-		if (!(sessionID in collaborations)) {
-			collaborations[sessionID] = {
-				'participants': []
-			}
+		// add socket.id to corresponding collaboration sessions
+		if (sessionID in collaborations){
+			collaborations[sessionID]['participants'].push(socket.id);
+		} else {
+			redisClient.get(sessionPath + sessionID, function(data){
+				if(data){
+					console.log('session terminated previously, pulling back...');
+					collaborations[sessionID] = {
+						'cachedInstructions' : JSON.parse(data),
+						'participants' : []
+					}
+				}else{
+					console.log('Nobody did this before, creating new session');
+					collaborations[sessionID] = {
+						'cachedInstructions' : [],
+						'participants' : []
+					}
+				}
+				collaborations[sessionID]['participants'].push(socket.id);
+			});
 		}
 
-		collaborations[sessionID]['participants'].push(socket.id);
-
-	// 	// add socket.id to corresponding collaboration sessions
-	// 	if(sessionID in collaborations){
-	// 		collaborations[sessionID]['participants'].push(socket.id);
-	// 	}else{
-	// 		redisClient.get(sessionPath + sessionID, function(data){
-	// 			if(data){
-	// 				console.log('session terminated previously, pulling back...');
-	// 				collaborations[sessionID] = {
-	// 					'cachedInstructions' : JSON.parse(data),
-	// 					'participants' : []
-	// 				}
-	// 			}else{
-	// 				console.log('Nobody did this before, creating new session');
-	// 				collaborations[sessionID] = {
-	// 					'cachedInstructions' : [],
-	// 					'participants' : []
-	// 				}
-	// 			}
-	// 			collaborations[sessionID]['participants'].push(socket.id);
-	// 		});
-	// 	}
-  //
-  //   // collaborations[sessionID]['participants'].push(socket.id);
+    // collaborations[sessionID]['participants'].push(socket.id);
 
 		//add change event listener
 		socket.on('change', (changeInEditor) => {
+			let sessionID = socketIDtoSessionID[socket.id];
+			if(sessionID in collaborations){
+				collaborations[sessionID]['cachedInstructions'].push(
+					['change', changeInEditor, Date.now()]
+				);
+			} else{
+				console.log('sessionID is not in collaborations');
+			}
 			forwardEvent(socket.id, 'change', changeInEditor);
 		});
 
@@ -70,33 +70,33 @@ module.exports = function(io){
       }
     });
 
-  // 	socket.on('disconnect', () => {
-	// 		var sessionID = socketIDtoSessionID[socket.id];
-	// 		console.log('socket '+ socket.id + ' disconnected from session ' + sessionID);
-	// 		var foundAndRemoved = false;
-	// 		if(sessionID in collaborations){
-	// 			var participants = collaborations[sessionID]['participants'];
-	// 			var index = participants.indexOf(socket.id);
-	// 			if(index >= 0){
-	// 				participants.splice(index, 1);
-	// 				foundAndRemoved = true;
-  //
-	// 				if(participants.length===0){
-	// 					console.log('last participant left, saving to Redis');
-	// 					let key = sessionPath + sessionID;
-	// 					let value = JSON.stringify(collaborations[sessionID]['cachedInstructions']);
-  //
-	// 					redisClient.set(key, value, redisClient.redisPrint);
-	// 					redisClient.expire(key, TIMEOUT_IN_SECONDS);
-	// 					delete collaborations[sessionID];
-	// 				}
-	// 			}
-	// 		}
-	// 		if(!foundAndRemoved){
-	// 			console.log('cannot find and remove participant in participants');
-	// 		}
-	// 	});
-  //
+  	socket.on('disconnect', () => {
+			var sessionID = socketIDtoSessionID[socket.id];
+			console.log('socket '+ socket.id + ' disconnected from session ' + sessionID);
+			var foundAndRemoved = false;
+			if(sessionID in collaborations){
+				var participants = collaborations[sessionID]['participants'];
+				var index = participants.indexOf(socket.id);
+				if(index >= 0){
+					participants.splice(index, 1);
+					foundAndRemoved = true;
+
+					if(participants.length===0){
+						console.log('last participant left, saving to Redis');
+						let key = sessionPath + sessionID;
+						let value = JSON.stringify(collaborations[sessionID]['cachedInstructions']);
+
+						redisClient.set(key, value, redisClient.redisPrint);
+						redisClient.expire(key, TIMEOUT_IN_SECONDS);
+						delete collaborations[sessionID];
+					}
+				}
+			}
+			if(!foundAndRemoved){
+				console.log('cannot find and remove participant in participants');
+			}
+		});
+
     var forwardEvent = function(socketID, eventName, dataString) {
     	let sessionID = socketIDtoSessionID[socketID];
 
