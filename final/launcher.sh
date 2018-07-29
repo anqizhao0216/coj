422ce97f16ef416420296d87982c4b1e30722604
Ubuntu <ubuntu@ip-172-31-30-12.us-east-2.compute.internal>
2018-07-29 01:43:15 +0000

    aws

--- final/launcher.sh
+++ final/launcher.sh
@@ -1,15 +1,15 @@
 fuser -k 3000/tcp
 fuser -k 5000/tcp
 
-service redis_6379 start
+sudo service redis_6379 start
 cd ./oj-server
-npm install
-nodemon server.js &
-cd ../oj-client
-npm install
-ng build --watch &
+#npm install
+sudo node server.js &
+#cd ../oj-client
+#npm install
+#ng build  &
 cd ../executor
-pip install -r requirements.txt
+#sudo pip install -r requirements.txt
 sudo python executor_server.py &
 
 echo "=================================================="
