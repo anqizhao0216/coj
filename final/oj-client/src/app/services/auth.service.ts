45efac983fa148490d14f78dfcbd351ec278d7af
Anqi <anqizhao0216@gmail.com>
2018-08-17 20:24:43 -0700

    Update auth.service.ts

--- final/oj-client/src/app/services/auth.service.ts
+++ final/oj-client/src/app/services/auth.service.ts
@@ -17,11 +17,11 @@ export class AuthService {
   userProfile = new BehaviorSubject<any>(undefined);
 
   auth0 = new auth0.WebAuth({
-    clientID: 'e7OifzSmOcWTLF7ydbk23CFGb5PpzY4v',
-    domain: 'coj531.auth0.com',
+    clientID: '3PHKJzzKW0DfKLFpckQ9YNsYWdz5Kq9k',
+    domain: 'coj0818.auth0.com',
     responseType: 'token id_token',
-    audience: 'https://coj531.auth0.com/userinfo',
-    redirectUri: 'http://localhost:3000/',
+    audience: 'https://coj0818.auth0.com/userinfo',
+    redirectUri: 'http://localhost:3000/callback',
     scope: 'openid profile read:messages write:messages'
   });
 
